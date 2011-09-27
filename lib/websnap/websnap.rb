52cdcef17e64ec1b3ae4153a07d40a844ab7a189
Amit Solanki <amitkssolanki@gmail.com>
2011-09-27 15:48:33 +0530

    * Added support to include external stylesheets * Added support to provide image path when external stylesheets are included

--- lib/websnap/websnap.rb
+++ lib/websnap/websnap.rb
@@ -56,8 +56,26 @@ module WebSnap
       return result
     end
   
-    def to_file(path)
+    def to_file(path, image_path=nil)
+      append_stylesheets
+      @source = Source.new(@source.to_s.gsub(/images\//, image_path)) unless image_path.nil?
       File.open(path,'w') {|file| file << self.to_bytes}
+    end
+  
+    def append_stylesheets
+      raise ImproperSourceError.new('Stylesheets may only be added to an HTML source') if stylesheets.any? && !@source.html?
+
+      stylesheets.each do |stylesheet|
+        if @source.to_s.match(/<\/head>/)
+          @source = Source.new(@source.to_s.gsub(/(<\/head>)/, style_tag_for(stylesheet)+'\1'))
+        else
+          @source.to_s.insert(0, style_tag_for(stylesheet))
+        end
+      end
+    end
+  
+    def style_tag_for(stylesheet)
+      "<style>#{File.read(stylesheet)}</style>"
     end
   
     protected
